90309c731b247b888da77480f606893762dbc290
veritozy <121317594+veritozy@users.noreply.github.com>
2026-07-21 04:04:18 +0000

    ip pùblica en el servicio

--- src/app/services/libro-service.ts
+++ src/app/services/libro-service.ts
@@ -9,7 +9,7 @@ import { Libro } from '../model/libro';
 export class LibroService {
   private http = inject(HttpClient);
 
-  private API_LIBROS = 'http://54.86.247.115:8080/libros';
+  private API_LIBROS = 'http://54.174.237.197:8080/libros';
 
   //Método GET
   getLibros():Observable<Libro[]>{
