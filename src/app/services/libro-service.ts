32fed18c605ff3de3aa3915ac295743300ffb48b
veritozy <121317594+veritozy@users.noreply.github.com>
2026-07-29 19:45:00 -0500

    Update libro-service.ts

--- src/app/services/libro-service.ts
+++ src/app/services/libro-service.ts
@@ -9,7 +9,7 @@ import { Libro } from '../model/libro';
 export class LibroService {
   private http = inject(HttpClient);
 
-  private API_LIBROS = 'http://98.84.165.64:8080/libros';
+  private API_LIBROS = 'http://98.93.227.175:8080/libros';
 
   //Método GET
   getLibros():Observable<Libro[]>{
